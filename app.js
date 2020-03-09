01fb2fd2104537abe48e9cfb22184368e40eecf4
rakimsth <raktim@rumsan.com>
2020-03-10 01:22:22 +0545

    adding 404 page and cleanup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 //personalization
 const config = require("config");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 var routeManager = require("./routes");
 var app = express();
 //personalization
@@ -14,6 +15,7 @@ mongoose.connect(config.get("app.db"), { useNewUrlParser: true, useUnifiedTopolo
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,9 +24,19 @@ app.use(express.static(path.join(__dirname, "public")));
 //personalization
 app.use("/", routeManager);
 
-// catch 404 and forward to error handler
+// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  if (req.headers["content-type"] == "application/json") {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    res.status(404).render("misc/404", {
+      title: "Not Found",
+      status: 404,
+      url: req.url
+    });
+  }
 });
 
 // error handler
@@ -35,7 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("misc/error");
 });
 
 module.exports = app;
